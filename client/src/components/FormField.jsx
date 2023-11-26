5a9410756f051190d23ba229f639e235add6f6c2
Kernel-rb <matabsaifeddine@gmail.com>
2023-11-26 20:10:43 +0100

    update components & Pages

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
 
-function FormField() {
+function FormField({ label, placeholder, type, name, value, handleChange, isSupriseMe, handleSupriseMe}) {
   return (
-    <div>FormField</div>
+    <div>
+    <div className='flex items-center gap-2 mb-2'>
+      <label 
+      htmlFor={name}
+      className='block text-sm font-medium tetx-gray-900'
+      >
+        {label}
+      </label>
+      {isSupriseMe && (
+        <button
+        type='button'
+        className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+        onClick={handleSupriseMe}
+        >
+          Surprise Me
+        </button>
+      )}
+    </div>
+    <input
+    type={type}
+    id={name}
+    name={name} 
+    placeholder={placeholder}
+    value={value}
+    onChange={handleChange}
+    required
+    className='
+    bg-gray-50
+    border
+    border-gray-300
+    text-gray-900
+    text-sm
+    rounded-lg
+    focus:ring-[#4649ff]
+    focus:border-[#4649ff]
+    outline-none
+    block
+    w-full
+    p-3
+    '
+    />
+    </div>
   )
 }
 
